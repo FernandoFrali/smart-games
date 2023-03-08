fbcf9a61d7852fa5bd02734721a9fd59b719a235
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-03-08 20:52:23 -0300

    :bug: chore: God of War III and Watch Dogs 2 links on games.xlsx fixed

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const connection = require('./database/database');
+const Game = require('./models/Game');
 const GameTable = require('./models/Game');
 const GameData = require('./seeders/Game');
+
+app.use(cors());
 
 connection
   .authenticate()
@@ -16,6 +20,17 @@ connection
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(6000, () => {
+app.get('/', (req, res) => {
+  res.send('ola');
+});
+
+app.get('/games', (req, res) => {
+  Game.findAll({ raw: true }).then((games) => {
+    res.statusCode = 200;
+    res.json(games);
+  });
+});
+
+app.listen(6060, () => {
   console.log('API online!');
 });
--- back-end/seeders/Game.js
+++ back-end/seeders/Game.js
@@ -7,11 +7,20 @@ const sheetName = workbook.SheetNames[0];
 const worksheet = workbook.Sheets[sheetName];
 
 const gamesFromExcel = XLSX.utils.sheet_to_json(worksheet);
-console.log('AQUI OLHA, OLHA OLHA:', gamesFromExcel);
 
-Game.bulkCreate(gamesFromExcel)
+Game.count()
+  .then((count) => {
+    if (count === 0) {
+      return Game.bulkCreate(gamesFromExcel);
+    } else {
+      console.log('A tabela games já possui registros.');
+      return null;
+    }
+  })
   .then((createdGames) => {
-    console.log(`Foram inseridos ${createdGames.length} jogos.`);
+    if (createdGames) {
+      console.log(`Foram inseridos ${createdGames.length} jogos.`);
+    }
   })
   .catch((error) => {
     console.error(error);
