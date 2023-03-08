cb3469148fb5260b1299a41fb6971d25e43f476b
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-03-08 05:37:06 -0300

    :sparkles: feat: back-end (API) finalizado e front-end iniciado / Header do site completa

---
+++ front-end/smartgames/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from './components/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Xesquedele</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/smartgames/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import games from './games.jpg';
+
+export const Header: React.FC = () => {
+  return (
+    <>
+      <div className="header">
+        <h1 className="logo">Smart Games</h1>
+        <input className="search" type="text" placeholder="Pesquise aqui..." />
+      </div>
+      <img src={games} alt="Diversos consoles" className="bigImg" />
+    </>
+  );
+};
