f33b865b42f822daf43a8b50aa8b235f27154f5d
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-03-08 08:37:37 -0300

    :sparkles: feat: Criado modal ao clicar no botão para comprar jogo

--- front-end/smartgames/src/components/Games/index.tsx
+++ front-end/smartgames/src/components/Games/index.tsx
@@ -1,16 +1,68 @@
 import React from 'react';
 import '../../styles/global.css';
+import Modal from 'react-modal';
 
 interface Game {
   name: string;
   urlImg: string;
   platforms: string;
   price: number;
+  description: string;
 }
 
+interface Subtitle {
+  style: {
+    color: string;
+  };
+}
+
+interface ModalStyles {
+  content: React.CSSProperties;
+  overlay: React.CSSProperties;
+}
+
+const customStyles: ModalStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#161313',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: '#161313',
+    WebkitOverflowScrolling: 'touch',
+    borderRadius: '4px',
+    outline: 'none',
+    padding: '20px',
+    display: 'grid',
+    justifyItems: 'center',
+    gap: '10px',
+  },
+};
+Modal.setAppElement('#root');
 export const Games: React.FC = () => {
+  let subtitle: Subtitle | null;
   const [games, setGames] = React.useState<Game[] | null>([]);
   const [error, setError] = React.useState<string | null>(null);
+  const [selectedGameIndex, setSelectedGameIndex] = React.useState<
+    number | null | false
+  >(null);
+
+  const afterOpenModal = () => {
+    if (subtitle) subtitle.style.color = 'blue';
+  };
+
+  const closeModal = () => {
+    setSelectedGameIndex(false);
+  };
 
   const fetchData = async (): Promise<void> => {
     try {
@@ -28,16 +80,51 @@ export const Games: React.FC = () => {
     fetchData();
   }, []);
 
+  const handleBuy = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const game = event?.currentTarget.id;
+    alert(`Compra concluída com sucesso! Você comprou: ${game}`);
+  };
+
   return (
     <div className="game-container">
       {games ? (
-        games.map((game) => (
+        games.map((game, index) => (
           <div className="game-card">
             <h2 className="game-title">{game.name}</h2>
             <img className="game-img" src={game.urlImg} />
             <p className="game-plat">{game.platforms}</p>
             <p className="game-price">R$ {game.price}</p>
-            <button className="buy-btn">Comprar</button>
+            <div>
+              <button
+                className="buy-btn"
+                onClick={() => setSelectedGameIndex(index)}
+              >
+                Comprar
+              </button>
+              <Modal
+                isOpen={selectedGameIndex === index}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={() => setSelectedGameIndex(null)}
+                style={customStyles}
+              >
+                <button onClick={closeModal} className="close-modal">
+                  Fechar
+                </button>
+                <h2 className="modal-title">{game.name}</h2>
+                <img src={game.urlImg} />
+                <p className="game-desc">{game.description}</p>
+                <div className="map">
+                  <p>Onde comprar:</p>
+                </div>
+                <button
+                  className="buy-modal"
+                  id={game.name}
+                  onClick={handleBuy}
+                >
+                  Comprar Jogo
+                </button>
+              </Modal>
+            </div>
           </div>
         ))
       ) : (
