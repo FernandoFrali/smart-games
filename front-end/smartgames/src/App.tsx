0055aec1a2e95a61ad77da845cb26783be212edd
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-03-08 23:03:02 -0300

    :sparkles: feat: Created post method to send the game name and game id when a user buys

--- front-end/smartgames/src/App.tsx
+++ front-end/smartgames/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Games } from './components/Games/index';
 import { Header } from './components/Header/index';
 import { GlobalStorage } from './GlobalContext';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
