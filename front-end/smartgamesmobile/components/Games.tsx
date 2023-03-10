6d1a978cf27c8a20bab510915f48927c7f081379
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-03-10 16:59:09 -0300

    :sparkles: Added Game component, which renders all games from database on screen (homepage and modal)

--- front-end/smartgamesmobile/components/Games.tsx
+++ front-end/smartgamesmobile/components/Games.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Modal,
+  Button,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+  Pressable,
+} from 'react-native';
+
+interface IStyles {
+  container: ViewStyle;
+  gameContainer: ViewStyle;
+  text: TextStyle;
+  smallTexts: TextStyle;
+  image: ImageStyle;
+  modalContainer: ViewStyle;
+  modalContent: ViewStyle;
+  modalText: TextStyle;
+}
+
+interface IGame {
+  id: number;
+  name: string;
+  platforms: string;
+  price: number;
+  urlImg: string;
+  description: string;
+}
 
 export const Games: React.FunctionComponent = () => {
-  const [games, setGames] = React.useState(null);
+  const [games, setGames] = React.useState<IGame[]>([]);
+  const [isVisible, setVisible] = React.useState(false);
+  const [error, setError] = React.useState('');
+
+  const [selectedGameIndex, setSelectedGameIndex] = React.useState<
+    number | null | false
+  >(null);
+
+  const closeModal = () => {
+    setSelectedGameIndex(false);
+    setVisible(false);
+  };
 
   const fetchData = async () => {
     const response = await fetch('http://localhost:6060/games');
@@ -15,28 +58,116 @@ export const Games: React.FunctionComponent = () => {
     fetchData();
   }, []);
 
+  const postData = async (game: string, id: number) => {
+    try {
+      const response = await fetch('http://localhost:6060/purchased', {
+        method: 'POST',
+        body: JSON.stringify({
+          gameName: game,
+          gameId: id,
+        }),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      });
+      return response.json();
+    } catch (error) {
+      setError('Não foi possível realizar a compra');
+    }
+  };
+
+  const handleBuy = (id: number, name: string) => {
+    postData(name, id);
+    const buyAlert = error
+      ? error
+      : `Compra concluída com sucesso! Você comprou: ${name}
+      Seu protocolo é: ${id}`;
+    alert(buyAlert);
+  };
+
+  const styles = StyleSheet.create<IStyles>({
+    container: {
+      display: 'flex',
+      flex: 1,
+    },
+    gameContainer: {
+      alignItems: 'center',
+    },
+    text: {
+      marginTop: 20,
+      fontWeight: 'bold',
+    },
+    smallTexts: {
+      marginBottom: 5,
+    },
+    image: {
+      width: '50%',
+      height: 350,
+      borderRadius: 4,
+      marginBottom: 5,
+    },
+    modalContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+      backgroundColor: '#fff',
+      padding: 20,
+      borderRadius: 4,
+      alignItems: 'center',
+    },
+    modalText: {
+      fontSize: 18,
+      marginBottom: 10,
+    },
+  });
+
   return (
-    <View>
-      {games ? (
-        games.map((game) => (
-          <View key={game.id}>
-            <Text>{game.name}</Text>
+    <View style={styles.container}>
+      {games.length ? (
+        games.map((game, index) => (
+          <View style={styles.gameContainer} key={game.id}>
+            <Text style={styles.text}>{game.name}</Text>
             <Image
-              style={{
-                width: '100%',
-                height: 350,
-                borderRadius: 4,
-              }}
+              style={styles.image}
               source={{
                 uri: game.urlImg,
               }}
             />
             <Text>{game.platforms}</Text>
-            <Text>R$ {game.price}</Text>
+            <Text style={styles.smallTexts}>R$ {game.price}</Text>
+            <Button
+              title="Comprar"
+              color="#841584"
+              onPress={() => setSelectedGameIndex(index)}
+            />
+            <Modal
+              visible={selectedGameIndex === index}
+              onRequestClose={closeModal}
+              onDismiss={closeModal}
+            >
+              <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                  <Text style={styles.modalText}>{game.name}</Text>
+                  <Image
+                    style={styles.image}
+                    source={{
+                      uri: game.urlImg,
+                    }}
+                  />
+                  <Text style={styles.smallTexts}>{game.description}</Text>
+                  <Button
+                    title={`Comprar ${game.name}`}
+                    onPress={() => handleBuy(game.id, game.name)}
+                  />
+                  <Button title="Fechar" onPress={closeModal} />
+                </View>
+              </View>
+            </Modal>
           </View>
         ))
       ) : (
-        <Text>Não há jogos</Text>
+        <Text>{error}</Text>
       )}
     </View>
   );
